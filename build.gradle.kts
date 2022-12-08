e4cc1003c0df7b9ae15694fa9b55c35032ab1d90
Shubham Matta <cs14btech11032@gmail.com>
2022-12-09 02:12:18 +0900

    New dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.ir.backend.js.compile
 
 plugins {
 	id("org.springframework.boot") version "3.0.0"
@@ -7,6 +8,7 @@ plugins {
 	kotlin("plugin.spring") version "1.7.21"
 	kotlin("plugin.jpa") version "1.7.21"
 	id("com.avast.gradle.docker-compose") version "0.16.11"
+	id("org.flywaydb.flyway") version "7.15.0"
 
 }
 
@@ -35,11 +37,20 @@ dependencies {
 
 
 	implementation("mysql:mysql-connector-java:8.0.25")
+	implementation("org.mybatis:mybatis:3.5.11")
+	implementation("org.apache.logging.log4j:log4j-api-kotlin:1.2.0")
+	implementation("org.apache.logging.log4j:log4j-api:2.17.0")
+	implementation("org.apache.logging.log4j:log4j-core:2.17.0")
+
 
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+
+	// testing
+	testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
 }
 
 tasks.withType<KotlinCompile> {
@@ -55,3 +66,14 @@ tasks.withType<Test> {
 
 val TaskContainer.`composeUp`: TaskProvider<com.avast.gradle.dockercompose.tasks.ComposeUp>
 	get() = named<com.avast.gradle.dockercompose.tasks.ComposeUp>("composeUp")
+
+tasks {
+	create("flywayMigrateFund", org.flywaydb.gradle.task.FlywayMigrateTask::class.java) {
+		url = "jdbc:mysql://localhost/payments?createDatabaseIfNotExist=true&autoReconnect=true&useSSL=false"
+		user = "root"
+		password = "test"
+		locations = arrayOf("filesystem:src/main/resources/db/table")
+	}
+}
+
+
